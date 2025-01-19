488909762fcf8c971d0ff5dc6b8f34aac3399b91
Ankit Raj <147710734+ankit-raj00@users.noreply.github.com>
2025-01-19 11:43:39 +0530

    Update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,10 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
+    console.log("1")
     destination: function (req, file, cb) {
       cb(null, './public/temp')
+    console.log("2")
     },
     filename: function (req, file, cb) {
       
